30f0a3653fcac2115a375a511234ade11d72a9d1
Ben Williams <desertblade@gmail.com>
2023-07-11 15:17:51 -0700

    Had Min and Hour Backwards

--- create_cron.sh
+++ create_cron.sh
@@ -2,7 +2,7 @@
 
 if [ ${CRON_HOUR} ]
   then
-    echo ${CRON_HOUR} ${CRON_MIN} '* * * /opt/src/scripts/ezShareDownloader.sh 2>&1' >> /etc/cron.d/ezShare
+    echo ${CRON_MIN} ${CRON_HOUR} '* * * /opt/src/scripts/ezShareDownloader.sh 2>&1' >> /etc/cron.d/ezShare
     chmod 0644 /etc/cron.d/ezShare
     crontab /etc/cron.d/ezShare
     cron
